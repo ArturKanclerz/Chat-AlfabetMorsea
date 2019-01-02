b102758e29aa84d0a5f993e777244ebb74a118fa
Artur Kanclerz <jumperarti@gmail.com>
2019-01-02 22:35:32 +0100

    Dodanie elementów layoutu

--- client.py
+++ client.py
@@ -34,7 +34,8 @@ def on_closing(event=None):
     send()
 
 top = tkinter.Tk()
-top.title("Chat On!")
+top.geometry("1000x550")
+top.title("Chat - Kanclerz & Romaniuk")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()  # For the messages to be sent.
@@ -46,6 +47,20 @@ scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
+var = tkinter.IntVar()
+tapy = 170
+var.set(0)
+Radiobutton(text='Tekst', value=0, variable=var).place(x=840, y=180)
+Radiobutton(text='Dekodowanie', value=1, variable=var).place(x=840, y=205)
+Radiobutton(text='Kodowanie', value=2, variable=var).place(x=840, y=230)
+Radiobutton(text='Sygnaly dzwiekowe', value=3, variable=var).place(x=840, y=255)
+Radiobutton(text='Sygnaly graficzne', value=4, variable=var).place(x=840, y=280)
+
+imageLabel = Label(top, text="Wiadomosc graficzna", fg="black", font="Helvetica")
+imageLabel.place(x=-10, y=230, height=30, width=200)
+
+image = Button(font="Helvetica", bg="grey")
+image.place(x=40, y=290, height=100, width=100)
 
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
@@ -55,8 +70,8 @@ send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#Socket part
-HOST = '127.0.0.1' # Enter host of the server without inverted commas
+# Socket part
+HOST = '127.0.0.1'  # Enter host of the server without inverted commas
 PORT = 33000
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
